7a55566f85f0a2e08836ea056950e9397e540b4a
Clarence Bunting <cbbunting1@my.waketech.edu>
2025-11-24 09:32:18 -0500

    Javadoc Comments Added

--- JavadocPractice.java
+++ JavadocPractice.java
@@ -1,14 +1,17 @@
 public class JavadocPractice {
 
-    // ------------------------------------------------------------
-    // TODO: Write a class-level Javadoc comment here.
-    // Describe the purpose of this class in 1–2 sentences.
-    // ------------------------------------------------------------
+    /**
+     * Contains various methods mainly methodsthat modify and check data for integers.
+     *Runs all of the methods to ensure that they function corrctly.
+     * 
+     */
 
 
     /**
-     * TODO: Write a full Javadoc block for this method.
-     * This is your program's entry point.
+     * Runs all the methods to test them and show how they work.
+     * Program's entry pint. Runs when you run program.
+     * 
+     * @param args Coomand Line Arguments
      */
     public static void main(String[] args) {
         printWelcome();
@@ -20,8 +23,12 @@ public class JavadocPractice {
 
 
     /**
-     * TODO: Write the full Javadoc block for this method.
-     * Should include @param and @return tags.
+     * Adds two values together.
+     * 
+     * @param a first input value for adding
+     * @param b second input value to be added to the first.
+     * @return what those two values added together is equal to.
+     * 
      */
     public static int add(int a, int b) {
         return a + b;
@@ -29,8 +36,10 @@ public class JavadocPractice {
 
 
     /**
-     * TODO: Write the full Javadoc block for this method.
-     * Should include @param and @return tags.
+     * Determines is a value is even
+     * 
+     * @param value value that you want to check whether it is even
+     * @return true if value is even; false if odd
      */
     public static boolean isEven(int value) {
         return value % 2 == 0;
@@ -38,8 +47,7 @@ public class JavadocPractice {
 
 
     /**
-     * TODO: Write the full Javadoc block for this method.
-     * This method takes no parameters and does not return anything.
+     * Displays a welcome message.
      */
     public static void printWelcome() {
         System.out.println("Welcome to the Javadoc practice file.");
@@ -47,8 +55,11 @@ public class JavadocPractice {
 
 
     /**
-     * TODO: Write the full Javadoc block for this method.
-     * Should include @param and @return tags.
+     * Determines the maximum of two integer values.
+     * 
+     * @param x First input value to check.
+     * @param y Second input value to check.
+     * @return Which of the input values is greater.
      */
     public static int max(int x, int y) {
         return (x > y) ? x : y;
